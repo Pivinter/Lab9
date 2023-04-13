682553d82b13c7ee4f6d65fae8aa499bab647faa
Vladuslav Kuliavets <70973362+Pivinter@users.noreply.github.com>
2023-04-13 22:47:24 +0300

    Add files via upload

--- Lab9.py
+++ Lab9.py
@@ -1,35 +1,32 @@
-class Node:
+class TreeNode:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
-
-def insert(root, value):
+def insert_node(root, value):
     if root is None:
-        return Node(value)
-
-    if value < root.value:
-        root.left = insert(root.left, value)
+        return TreeNode(value)
     else:
-        root.right = insert(root.right, value)
-
+        if value < root.value:
+            root.left = insert_node(root.left, value)
+        else:
+            root.right = insert_node(root.right, value)
     return root
 
-
-def print_tree(root, level=0):
-    if root is not None:
-        print_tree(root.left, level + 1)
-        print(" " * 4 * level + "->", root.value)
-        print_tree(root.right, level + 1)
-
-
-def build_binary_search_tree(elements):
+def build_tree(num_nodes):
+    import random
     root = None
-    for element in elements:
-        root = insert(root, element)
+    for _ in range(num_nodes):
+        root = insert_node(root, random.randint(1, 100))
     return root
 
+def print_tree(root, prefix=""):
+    if root is None:
+        return
+    print_tree(root.left, prefix + "--")
+    print(prefix + str(root.value))
+    print_tree(root.right, prefix + "--")
 
 def count_leaf_nodes(root):
     if root is None:
@@ -38,32 +35,238 @@ def count_leaf_nodes(root):
         return 1
     return count_leaf_nodes(root.left) + count_leaf_nodes(root.right)
 
+def remove_node_with_no_children(root, value):
+    if root is None:
+        return root
+    
+    if value < root.value:
+        root.left = remove_node_with_no_children(root.left, value)
+    elif value > root.value:
+        root.right = remove_node_with_no_children(root.right, value)
+    else:
+        if root.left is None and root.right is None:
+            root = None
+    return root
 
-def main():
-    while True:
-        print("\nМеню:")
-        print("1. Створити бінарне дерево пошуку")
-        print("2. Вивести бінарне дерево пошуку на екран")
-        print("3. Визначити кількість елементів, які не мають дочірніх")
-        print("0. Вийти")
+def find_nodes_without_children(root, nodes=None):
+    if nodes is None:
+        nodes = []
 
-        choice = int(input("Введіть номер операції: "))
+    if root is None:
+        return nodes
 
-        if choice == 1:
-            n = int(input("Введіть кількість елементів: "))
-            elements = [int(input(f"Введіть елемент {i + 1}: ")) for i in range(n)]
-            root = build_binary_search_tree(elements)
-        elif choice == 2:
-            print("Бінарне дерево пошуку:")
-            print_tree(root)
-        elif choice == 3:
-            count = count_leaf_nodes(root)
-            print(f"Кількість елементів, які не мають дочірніх: {count}")
-        elif choice == 0:
-            break
+    if root.left is None and root.right is None:
+        nodes.append(root.value)
+
+    find_nodes_without_children(root.left, nodes)
+    find_nodes_without_children(root.right, nodes)
+
+    return nodes
+
+def remove_node_with_one_child(root, value):
+    if root is None:
+        return root
+
+    if value < root.value:
+        root.left = remove_node_with_one_child(root.left, value)
+    elif value > root.value:
+        root.right = remove_node_with_one_child(root.right, value)
+    else:
+        if root.left is None:
+            root = root.right
+        elif root.right is None:
+            root = root.left
+    return root
+
+def find_nodes_with_one_child(root, nodes=None):
+    if nodes is None:
+        nodes = []
+
+    if root is None:
+        return nodes
+
+    if (root.left is not None and root.right is None) or (root.left is None and root.right is not None):
+        nodes.append(root.value)
+
+    find_nodes_with_one_child(root.left, nodes)
+    find_nodes_with_one_child(root.right, nodes)
+
+    return nodes
+
+def find_min_value_node(node):
+    while node.left is not None:
+        node = node.left
+    return node
+
+def remove_node_with_two_children(root, value):
+    if root is None:
+        return root
+
+    if value < root.value:
+        root.left = remove_node_with_two_children(root.left, value)
+    elif value > root.value:
+        root.right = remove_node_with_two_children(root.right, value)
+    else:
+        if root.left and root.right:
+            temp = find_min_value_node(root.right)
+            root.value = temp.value
+            root.right = remove_node_with_no_children(root.right, temp.value)
+    return root
+
+def find_nodes_with_two_children(root, nodes=None):
+    if nodes is None:
+        nodes = []
+
+    if root is None:
+        return nodes
+
+    if root.left is not None and root.right is not None:
+        nodes.append(root.value)
+
+    find_nodes_with_two_children(root.left, nodes)
+    find_nodes_with_two_children(root.right, nodes)
+
+    return nodes
+
+def remove_branch(root, value):
+    if root is None:
+        return root
+
+    if value < root.value:
+        root.left = remove_branch(root.left, value)
+    elif value > root.value:
+        root.right = remove_branch(root.right, value)
+    else:
+        root = None
+    return root
+
+def find_all_nodes(root, nodes=None):
+    if nodes is None:
+        nodes = []
+
+    if root is None:
+        return nodes
+
+    nodes.append(root.value)
+
+    find_all_nodes(root.left, nodes)
+    find_all_nodes(root.right, nodes)
+
+    return nodes
+
+def tree_to_dict(root):
+    if root is None:
+        return None
+    return {
+        'value': root.value,
+        'left': tree_to_dict(root.left),
+        'right': tree_to_dict(root.right)
+    }
+
+def dict_to_tree(tree_dict):
+    if tree_dict is None:
+        return None
+    root = TreeNode(tree_dict['value'])
+    root.left = dict_to_tree(tree_dict['left'])
+    root.right = dict_to_tree(tree_dict['right'])
+    return root
+
+def save_tree_to_file_preorder(root, file):
+    if root is None:
+        file.write("-1 ")
+        return
+    file.write(str(root.value) + " ")
+    save_tree_to_file_preorder(root.left, file)
+    save_tree_to_file_preorder(root.right, file)
+
+def save_tree_to_file(root, filename):
+    with open(filename, 'w') as file:
+        save_tree_to_file_preorder(root, file)
+
+def load_tree_from_file_preorder(file):
+    value = int(next(file))
+    if value == -1:
+        return None
+
+    root = TreeNode(value)
+    root.left = load_tree_from_file_preorder(file)
+    root.right = load_tree_from_file_preorder(file)
+    return root
+
+def load_tree_from_file(filename):
+    with open(filename, 'r') as file:
+        return load_tree_from_file_preorder((val for val in file.read().split()),)
+
+
+
+print("1. Створити бінарне дерево")
+print("2. Показати бінарне дерево")
+print("3. Показати Кількість елементів без дочірніх вузлів")
+print("4. Видалити вузол без дочірніх вузлів")
+print("5. Видалити вузол з одним дочірнім вузлом")
+print("6. Видалити вузол з двома дочірніми вузлами")
+print("7. Видалити гілку дерева")
+print("8. Зберегти дерево у файл")
+print("9. Завантажити дерево з файлу")
+print("0. Вихід")
+root = None
+while True:
+    choice = int(input("Введіть номер опції: "))
+    if choice == 1:
+        num_nodes = int(input("Введіть кількість елементів: "))
+        root = build_tree(num_nodes)
+        print("Бінарне дерево створено.")
+    elif choice == 2:
+        print("Бінарне дерево:")
+        print_tree(root)
+    elif choice == 3:
+        count = count_leaf_nodes(root)
+        print(f"Кількість елементів без дочірніх вузлів: {count}")
+    elif choice == 4:
+        nodes_without_children = find_nodes_without_children(root)
+        if nodes_without_children:
+            print("Вузли без дочірніх вузлів: ", ", ".join(map(str, nodes_without_children)))
+            value = int(input("Введіть значення вузла, який хочете видалити: "))
+            root = remove_node_with_no_children(root, value)
+            print("Вузол без дочірніх вузлів видалено, якщо він існував.")
         else:
-            print("Невірний вибір, спробуйте ще раз.")
-
-
-if __name__ == "__main__":
-    main()
+            print("Немає вузлів без дочірніх елементів для видалення.")
+    elif choice == 5:
+        nodes_with_one_child = find_nodes_with_one_child(root)
+        if nodes_with_one_child:
+            print("Вузли з одним дочірнім вузлом: ", ", ".join(map(str, nodes_with_one_child)))
+            value = int(input("Введіть значення вузла, який хочете видалити: "))
+            root = remove_node_with_one_child(root, value)
+            print("Вузол з одним дочірнім вузлом видалено, якщо він існував.")
+        else:
+            print("Немає вузлів з одним дочірнім елементом для видалення.")
+    elif choice == 6:
+        nodes_with_two_children = find_nodes_with_two_children(root)
+        if nodes_with_two_children:
+            print("Вузли з двома дочірніми вузлами: ", ", ".join(map(str, nodes_with_two_children)))
+            value = int(input("Введіть значення вузла, який хочете видалити: "))
+            root = remove_node_with_two_children(root, value)
+            print("Вузол з двома дочірніми вузлами видалено, якщо він існував.")
+        else:
+            print("Немає вузлів з двома дочірніми елементами для видалення.")
+    elif choice == 7:
+        all_nodes = find_all_nodes(root)
+        if all_nodes:
+            print("Всі вузли дерева: ", ", ".join(map(str, all_nodes)))
+            value = int(input("Введіть значення вузла, який хочете видалити: "))
+            root = remove_branch(root, value)
+            print("Гілка дерева видалена, якщо вона існувала.")
+        else:
+            print("Немає гілок для видалення.")
+    elif choice == 8:
+        filename = input("Введіть назву файлу: ")
+        save_tree_to_file(root, filename)
+        print("Дерево збережено у файл.")
+    elif choice == 9:
+        filename = input("Введіть назву файлу: ")
+        root = load_tree_from_file(filename)
+        print("Дерево завантажено з файлу.")
+    elif choice == 0:
+        break
+    else:
+        print("Неправильний вибір, спробуйте ще раз.")
--- Unittest9.py
+++ Unittest9.py
@@ -1,41 +1,87 @@
 import unittest
-from Lab9 import Node, insert, build_binary_search_tree, count_leaf_nodes
+import random
+from Lab9 import build_tree, count_leaf_nodes, dict_to_tree, find_nodes_with_one_child, find_nodes_with_two_children, find_nodes_without_children, insert_node, load_tree_from_file, remove_node_with_no_children, remove_node_with_one_child, remove_node_with_two_children, save_tree_to_file, tree_to_dict
 
 
-class TestNode(unittest.TestCase):
+class TestTreeNodeFunctions(unittest.TestCase):
 
-    def test_node_init(self):
-        node = Node(5)
-        self.assertEqual(node.value, 5)
-        self.assertIsNone(node.left)
-        self.assertIsNone(node.right)
+    def setUp(self):
+        self.root = build_tree(10)
 
-    def test_insert(self):
-        root = None
-        root = insert(root, 5)
-        root = insert(root, 3)
-        root = insert(root, 7)
-        self.assertEqual(root.value, 5)
-        self.assertEqual(root.left.value, 3)
-        self.assertEqual(root.right.value, 7)
+    def test_insert_node(self):
+        tree = None
+        values = [random.randint(1, 100) for _ in range(5)]
 
-    def test_build_binary_search_tree(self):
-        elements = [5, 3, 7, 1, 4, 6, 8]
-        root = build_binary_search_tree(elements)
-        self.assertEqual(root.value, 5)
-        self.assertEqual(root.left.value, 3)
-        self.assertEqual(root.right.value, 7)
-        self.assertEqual(root.left.left.value, 1)
-        self.assertEqual(root.left.right.value, 4)
-        self.assertEqual(root.right.left.value, 6)
-        self.assertEqual(root.right.right.value, 8)
+        for value in values:
+            tree = insert_node(tree, value)
+
+        for value in values:
+            self.assertIsNotNone(self.find_node(tree, value))
+
+    def find_node(self, root, value):
+        if root is None:
+            return None
+
+        if root.value == value:
+            return root
+
+        if value < root.value:
+            return self.find_node(root.left, value)
+        else:
+            return self.find_node(root.right, value)
 
     def test_count_leaf_nodes(self):
-        elements = [5, 3, 7, 1, 4, 6, 8]
-        root = build_binary_search_tree(elements)
-        count = count_leaf_nodes(root)
-        self.assertEqual(count, 4)
+        leaf_nodes_count = count_leaf_nodes(self.root)
+        self.assertGreaterEqual(leaf_nodes_count, 1)
 
+    def test_remove_node_with_no_children(self):
+        nodes_without_children = find_nodes_without_children(self.root)
+
+        if nodes_without_children:
+            value_to_remove = nodes_without_children[0]
+            self.root = remove_node_with_no_children(self.root, value_to_remove)
+            self.assertIsNone(self.find_node(self.root, value_to_remove))
+
+    def test_find_nodes_without_children(self):
+        nodes_without_children = find_nodes_without_children(self.root)
+        for node_value in nodes_without_children:
+            node = self.find_node(self.root, node_value)
+            self.assertIsNone(node.left)
+            self.assertIsNone(node.right)
+
+    def test_remove_node_with_one_child(self):
+        nodes_with_one_child = find_nodes_with_one_child(self.root)
+
+        if nodes_with_one_child:
+            value_to_remove = nodes_with_one_child[0]
+            self.root = remove_node_with_one_child(self.root, value_to_remove)
+            self.assertIsNone(self.find_node(self.root, value_to_remove))
+
+    def test_remove_node_with_two_children(self):
+        nodes_with_two_children = find_nodes_with_two_children(self.root)
+
+        if nodes_with_two_children:
+            value_to_remove = nodes_with_two_children[0]
+            self.root = remove_node_with_two_children(self.root, value_to_remove)
+            self.assertIsNone(self.find_node(self.root, value_to_remove))
+
+    def test_find_nodes_with_two_children(self):
+        nodes_with_two_children = find_nodes_with_two_children(self.root)
+        for node_value in nodes_with_two_children:
+            node = self.find_node(self.root, node_value)
+            self.assertIsNotNone(node.left)
+            self.assertIsNotNone(node.right)
+
+    def test_tree_to_dict(self):
+        tree_dict = tree_to_dict(self.root)
+        self.assertIsNotNone(tree_dict)
+        self.assertEqual(tree_dict["value"], self.root.value)
+
+    def test_dict_to_tree(self):
+        tree_dict = tree_to_dict(self.root)
+        new_root = dict_to_tree(tree_dict)
+        self.assertIsNotNone(new_root)
+        self.assertEqual(new_root.value, self.root.value)
 
 if __name__ == '__main__':
     unittest.main()
